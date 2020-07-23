8be73528663514683a48af2080ec1f507a6273ab
Zhanserik Kenges <zhans.kenes@gmail.com>
2020-07-24 01:48:44 +0600

    Initial Commit

---
+++ FlipCardsGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlipCardsGame
+//
+//  Created by Zhanserik Kenes on 7/24/20.
+//  Copyright © 2020 Zhanserik Kenes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
